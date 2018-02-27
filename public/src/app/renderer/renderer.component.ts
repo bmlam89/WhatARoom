d23c3d70f940fda7f9f889b348e8b7812846114b
bmlam-89 <bmlam89@gmail.com>
2018-02-26 18:47:19 -0800

    Added dummy data to test to see if each sofa would load its own unique data. Added sofa class to begin storing sofa information for front-end

--- public/src/app/renderer/renderer.component.ts
+++ public/src/app/renderer/renderer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sofa } from '../sofa'
 declare var $: any;
 declare var jQuery: any;
 @Component({
@@ -8,12 +9,40 @@ declare var jQuery: any;
 })
 export class RendererComponent implements OnInit {
 
-  constructor() { }
+  sofas = [];
+  data = new Sofa('1','2','3');
+  s1 = new Sofa('Angela II w/ wooden trim','A1', '$1199');
+  s2 = new Sofa('Olinee','A1','$1199');
+  s3 = new Sofa('Chandler','A1','$1299');
+  s4 = new Sofa('A014','A1','$1199');
+  s5 = new Sofa('Jasper Kent','A1','$1199');
+  s6 = new Sofa('Richmond','A1','$1199');
+  s7 = new Sofa('Abu','A1','$1199');
+  s8 = new Sofa('Angela II w/o wooden trim','A1','$1099');
+  s9 = new Sofa('Willow','Modway','$1199');
+  s10 = new Sofa('Carson','Modway','$1199');
+  s11 = new Sofa('Jasper','Modway','$1199');
+  s12 = new Sofa('Danny','Modus','$1199');
+  s13 = new Sofa('Tiffany','Modus','$1199');
+  s14 = new Sofa('Rio','Modus','$1199');
+  constructor() {
+  
+
+
+    
+   }
 
   ngOnInit() {
+    this.sofas.push(this.s1,this.s2,this.s3,this.s4,this.s5,this.s6,this.s7,this.s8,this.s9,this.s10,
+    this.s11,this.s12,this.s13,this.s14);
+
   }
 
-  load_modal(){
+  load_modal(sofa){
+    console.log(sofa.style);
+    this.data.style = sofa.style;
+    this.data.price = sofa.price;
+    this.data.manufacturer = sofa.manufacturer;
     $('#product_modal').show();
   }
 
---
+++ public/src/app/sofa.ts
@@ -0,0 +1,8 @@
+export class Sofa {
+    style; manufacturer; price;
+    constructor(style,manufacturer,price){
+        this.style = style;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+}
